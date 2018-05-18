ec5b481f072d8ba5142b87029c9a521e95ef6836
Levi Lemke <18.levi.lemke@ksd.org>
2018-05-18 13:54:12 -0700

    meh

--- testFolder/Assets/levi/scripts/save.cs
+++ testFolder/Assets/levi/scripts/save.cs
@@ -4,13 +4,64 @@ using UnityEngine;
 
 public class save : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    /*// Use this for initialization
+    void Start()
+    {
+        Load();
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Save"))
+        {
+            Save();
+            // Debug.Log("saved");
+        }
+    }
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/" + gameObject.name + SceneManager.GetActiveScene().name + "Arcade.dat");
+        Save info = new Save();
+        //Save myData = new Save();
+        if (sceneName == ("Game"))
+        {
+            info.x = transform.position.x;
+            info.y = transform.position.y;
+            info.z = transform.position.z;
+        }
+        info.playerProgress = PlayerPrefs.GetInt("Progress");
+        bf.Serialize(file, info);
+        file.Close();
+    }
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/" + gameObject.name + SceneManager.GetActiveScene().name + "Arcade.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/" + gameObject.name + "Arcade.dat", FileMode.Open);
+            Save info = (Save)bf.Deserialize(file);
+            if (sceneName == "Game")
+            {
+                transform.position = new Vector3(info.x, info.y, info.z);
+                PlayerPrefs.SetInt("Progress", info.playerProgress);
+            }
+            file.Close();
+        }
+    }
+
+}
+
+[Serializable]
+public class Save
+{
+    public float x;
+    public float y;
+    public float z;
+
+    public int playerProgress;  */
 }
