336a3b29f679e9d24cdeb5ad0c1c5d097d98eeb6
Levi Lemke <18.levi.lemke@ksd.org>
2018-06-04 13:54:09 -0700

    question

---
+++ testFolder/Assets/levi/scripts/potionPurchasing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class potionPurchasing : MonoBehaviour {
+    public float healthpotionval = 5;
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        GetComponent<playerMove>;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            
+        }
+    }
+}
+
+    
+
